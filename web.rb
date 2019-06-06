a2147535c2e36b61f6271a905557d10adaceca60
Grégoire Willmann <gregoire@kinoba.fr>
2019-06-06 16:36:48 +0200

    Fix missing Jenkins-Crumb header + Fix linter warnings

--- web.rb
+++ web.rb
@@ -1,21 +1,22 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'rest-client'
 require 'json'
 require 'slack-notifier'
 
 get '/' do
-  "This is a thing"
+  'This is a thing'
 end
 
 post '/' do
-
   # Verify all environment variables are set
-  return [403, "No slack token setup"] unless slack_token = ENV['SLACK_TOKEN']
-  return [403, "No jenkins url setup"] unless jenkins_url= ENV['JENKINS_URL']
-  return [403, "No jenkins token setup"] unless jenkins_token= ENV['JENKINS_TOKEN']
+  return [403, 'No slack token setup'] unless slack_token = ENV['SLACK_TOKEN']
+  return [403, 'No jenkins url setup'] unless jenkins_url = ENV['JENKINS_URL']
+  return [403, 'No jenkins token setup'] unless jenkins_token = ENV['JENKINS_TOKEN']
 
   # Verify slack token matches environment variable
-  return [401, "No authorized for this command"] unless slack_token == params['token']
+  return [401, 'No authorized for this command'] unless slack_token == params['token']
 
   # Split command text
   text_parts = params['text'].split(' ')
@@ -29,21 +30,26 @@ post '/' do
     all_params = text_parts[1..-1]
     all_params.each do |p|
       p_thing = p.split('=')
-      parameters << { :name => p_thing[0], :value => p_thing[1] }
+      parameters << { name: p_thing[0], value: p_thing[1] }
     end
   end
+
+  # Get Jenkins crumb
+  resp = RestClient.get("#{jenkins_url}/crumbIssuer/api/json")
+  resp_json = JSON.parse(resp.body)
+  crumb = resp_json['crumb']
 
   # Jenkins url
   jenkins_job_url = "#{jenkins_url}/job/#{job}"
 
   # Get next jenkins job build number
   resp = RestClient.get "#{jenkins_job_url}/api/json"
-  resp_json = JSON.parse( resp.body )
+  resp_json = JSON.parse(resp.body)
   next_build_number = resp_json['nextBuildNumber']
 
   # Make jenkins request
-  json = JSON.generate( {:parameter => parameters} )
-  resp = RestClient.post "#{jenkins_job_url}/build?token=#{jenkins_token}", :json => json
+  json = JSON.generate(parameter: parameters)
+  RestClient.post "#{jenkins_job_url}/build?token=#{jenkins_token}", json: json, 'Jenkins-Crumb': crumb
 
   # Build url
   build_url = "#{jenkins_job_url}/#{next_build_number}"
@@ -55,5 +61,4 @@ post '/' do
   end
 
   build_url
-
 end
